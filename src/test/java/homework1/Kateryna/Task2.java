f1fb766e7f195ea3e555ad6b03196adb564fca65
KYIV\Kateryna_Norenko <kateryna_norenko@epam.com>
2021-04-21 22:29:52 +0300

    resolves comments

--- src/test/java/homework1/Kateryna/Task2.java
+++ src/test/java/homework1/Kateryna/Task2.java
@@ -2,14 +2,26 @@ package homework1.Kateryna;
 
 public class Task2 {
     public static void main(String[] args) {
-    int d = 2, e = 3, f = 5, g = 6, h = 8, i = 9, j = 15, k = 20, l = 55, m, n, o, p;
-    m = -f + h * g;
-    n = (l + i) % i;
-    o = k + (-e) * f / h;
-    p = f + j / e * d - h % e;
-    System.out.println(m);
-    System.out.println(n);
-    System.out.println(o);
-    System.out.println(p);
+        int d = 2;
+        int e = 3;
+        int f = 5;
+        int g = 6;
+        int h = 8;
+        int i = 9;
+        int j = 15;
+        int k = 20;
+        int l = 55;
+        int m;
+        int n;
+        int o;
+        int p;
+        m = -f + h * g;
+        n = (l + i) % i;
+        o = k + (-e) * f / h;
+        p = f + j / e * d - h % e;
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
     }
 }
--- src/test/java/homework1/Kateryna/Task4.java
+++ src/test/java/homework1/Kateryna/Task4.java
@@ -2,11 +2,11 @@ package homework1.Kateryna;
 
 public class Task4 {
     public static void main(String[] args) {
-        int [] arr = new int [] {5,6,7,12,-5,32,43};
+        int[] arr = new int[]{5, 6, 7, 12, -5, 32, 43};
         int sum = 0;
         for (int j : arr) {
             sum += j;
         }
-        System.out.println(+ sum);
+        System.out.println(sum);
     }
 }
